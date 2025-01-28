7441453d00fbde317c02c102ea1710eaeb3e6f5a
arthurferreira <147673248+heyarrthur@users.noreply.github.com>
2025-01-28 10:25:28 -0300

    Add files via upload

---
+++ Plataforma de Acessos/script.js
@@ -0,0 +1,31 @@
+function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const type = passwordInput.type === 'password' ? 'text' : 'password';
+    passwordInput.type = type;
+}
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Lista de logins válidos com redirecionamento específico
+    const validLogins = [
+        { username: 'admin', password: '1234', redirect: 'dashadmin/index.html' },
+        { username: 'robison.ferreira@dex.co', password: 'Robisonribeiro', redirect: 'dash/index.html' },
+        { username: 'usuario', password: 'senha', redirect: 'dashusuario/index.html' }
+    ];
+
+    // Verifica se o login é válido
+    const user = validLogins.find(
+        (login) => login.username === username && login.password === password
+    );
+
+    if (user) {
+        // Redireciona para a página específica do usuário
+        window.location.href = user.redirect;
+    } else {
+        alert('Credenciais inválidas. Tente novamente.');
+    }
+});
